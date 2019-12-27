91f070c3f7457495d1521e156046958369f31091
JulieH <d24021097@gmail.com>
2019-12-27 11:18:27 +0800

    USA

--- driving.py
+++ driving.py
@@ -1,8 +1,13 @@
-contry = input('請輸入您的國家：')
+country = input('請輸入您的國家：')
 age = input('請輸入您的年齡：')
 age = int(age)
-if contry == '日本':
+if country == '日本':
 	if age >= 18:
+		print('你可以考駕照了喔')
+	else:
+		print('你還不能考駕照喔')
+elif country == '美國':
+	if age >= 16:
 		print('你可以考駕照了喔')
 	else:
 		print('你還不能考駕照喔')
